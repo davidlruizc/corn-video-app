4bd4ccc465f1310cfeb19f85ddbae678f2fe9be4
David Ruiz <davidlecodes@Davids-MacBook-Pro.local>
2019-09-15 19:34:50 -0500

    Gesture handler

--- android/app/src/main/java/com/platzivideoapp/MainApplication.java
+++ android/app/src/main/java/com/platzivideoapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.platzivideoapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
@@ -11,6 +12,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,10 +29,12 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
             new RNScreensPackage(),
             new ReanimatedPackage(),
             new RNCWebViewPackage(),
-            new ReactVideoPackage()
+            new ReactVideoPackage(),
+            new RNGestureHandlerPackage()
       );
     }
 
